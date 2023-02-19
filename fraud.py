8c1eca0314cd9c866b3967a4ad966d09ae800bf3
janejeshen <janenjuguna550@gmail.com>
2023-02-19 21:34:26 +0300

    completed

--- fraud.py
+++ fraud.py
@@ -3,9 +3,13 @@ import pandas as pd
 from sklearn.preprocessing import OneHotEncoder
 import pickle as pkl
 import base64
-
+import warnings
+warnings.filterwarnings('error')
+from sklearn.exceptions import DataConversionWarning
+warnings.filterwarnings(action='ignore', category=DataConversionWarning)
 
 st.set_page_config(page_title='Tunisia Electricity Fraud Detection', page_icon='🕵️', layout="wide", initial_sidebar_state="auto")
+
 def get_base64_of_bin_file(bin_file):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -51,21 +55,21 @@ st.header('Detecting Electricity Fraud In Tunisia')
 st.markdown('The Tunisian Company of Electricity and Gas, STEG, experienced large losses as a result of consumer fraud. My app analyzes usage patterns and flags suspect accounts using data modeling to find potential fraudsters in order to stop further losses.')
 st.write('##### ⚠️This is not the official Tunisian Company of Electricity and Gas app‼️')
 # Load your data
+
 data = pd.read_csv('/home/jane/Documents/electricity/Fraud-Detection-in-Electricity-and-Gas-Consumption-in-tunisia/data/agg_train.csv')
 
 # Identify categorical features
 cat_features = ['district', 'client_catg', 'region']
 
-# One-hot encode categorical features using get_dummies
-encoded = pd.get_dummies(data[cat_features])
+# One-hot encode categorical features
+encoder = OneHotEncoder()
+encoded = encoder.fit_transform(data[cat_features])
 
 # Combine encoded features with numerical features
 numerical_features = ['transactions_count', 'consommation_level_1_mean', 'consommation_level_2_mean', 'consommation_level_3_mean', 'consommation_level_4_mean']
-X = pd.concat([encoded, data[numerical_features]], axis=1)
-
+X = pd.concat([pd.DataFrame(encoded.toarray()), data[numerical_features]], axis=1)
 
 # Load the saved model using the load_model() function
-# @st.cache(allow_output_mutation=True)
 def load_model(path):
     with open(path, 'rb') as f:
         fraud_model = pkl.load(f)
@@ -73,7 +77,13 @@ def load_model(path):
 
 fraud_model = load_model('/home/jane/Documents/electricity/Fraud-Detection-in-Electricity-and-Gas-Consumption-in-tunisia/model.pkl')
 
-# Display the input form to collect user inputs
+#Dummy function for the model prediction
+def predict(district, client_catg, region, transactions_count, consommation_level_1_mean, consommation_level_2_mean, consommation_level_3_mean, consommation_level_4_mean):
+    # Insert your model prediction code here
+    # This is just a dummy function that returns the sum of the input features
+    return transactions_count + consommation_level_1_mean + consommation_level_2_mean + consommation_level_3_mean + consommation_level_4_mean
+
+# User input for features
 district = st.selectbox('District', data['district'].unique())
 client_catg = st.selectbox('Client Category', data['client_catg'].unique())
 region = st.selectbox('Region', data['region'].unique())
@@ -83,17 +93,12 @@ consommation_level_2_mean = st.number_input('Consommation Level 2 Mean', value=0
 consommation_level_3_mean = st.number_input('Consommation Level 3 Mean', value=0.0, step=0.1)
 consommation_level_4_mean = st.number_input('Consommation Level 4 Mean', value=0.0, step=0.1)
 
-# Create input data as a DataFrame
 input_data = pd.DataFrame([[district, client_catg, region, transactions_count, consommation_level_1_mean, consommation_level_2_mean, consommation_level_3_mean, consommation_level_4_mean]], columns=['district', 'client_catg', 'region', 'transactions_count', 'consommation_level_1_mean', 'consommation_level_2_mean', 'consommation_level_3_mean', 'consommation_level_4_mean'])
 
-# One-hot encode categorical features using get_dummies
-encoded_input = pd.get_dummies(input_data, columns=cat_features)
+encoded_input = encoder.transform(input_data[cat_features])
+input_features = pd.concat([pd.DataFrame(encoded_input.toarray()), input_data[numerical_features]], axis=1)
+prediction = fraud_model.predict(input_features.values)
 
-# Combine encoded features with numerical features
-input_features = pd.concat([encoded_input, input_data[numerical_features]], axis=1)
-
-# Make a prediction using the machine learning model
-prediction = fraud_model.predict(input_features)
 
 
 # button_style = 'background-color: green; color: white; font-weight: bold;'
@@ -101,13 +106,18 @@ if st.button("Predict", key='predict_button', help='Click here to make a predict
     with st.spinner("Making prediction..."):
         # Use the trained model to predict the outcome and display it to the user
         if prediction[0] == 0:
-           st.write('**Prediction:** The customer is not engaging in fraudulent activities.')
+            st.write('*Prediction:* The customer is not engaging in fraudulent activities.')
         else:
-            st.write('**Prediction:** The customer is engaging in fraudulent activities.')
+            st.write('*Prediction:* The customer is engaging in fraudulent activities.')
     st.success("Prediction completed!")
 
 
-# st.markdown('<div class="contact-container">', unsafe_allow_html=True)
+
+
+
+
+
+st.markdown('<div class="contact-container">', unsafe_allow_html=True)
 
 with st.container():
     st.write("### Contact Info")
