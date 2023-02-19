22067ef893152b01c3cc3b5f211473c3d373238c
Jane Njuguna <106226707+janejeshen@users.noreply.github.com>
2023-02-19 22:31:26 +0300

    Add files via upload

--- fraud.py
+++ fraud.py
@@ -41,7 +41,7 @@ st.markdown('The Tunisian Company of Electricity and Gas, STEG, experienced larg
 st.write('##### ⚠️This is not the official Tunisian Company of Electricity and Gas app‼️')
 # Load your data
 
-data = pd.read_csv('/home/jane/Documents/Fraud-Detection-in-Electricity-and-Gas-Consumption-in-tunisia/data/agg_train.csv')
+data = pd.read_csv('data/agg_train.csv')
 
 # Identify categorical features
 cat_features = ['district', 'client_catg', 'region']
@@ -55,7 +55,7 @@ numerical_features = ['transactions_count', 'consommation_level_1_mean', 'consom
 X = pd.concat([pd.DataFrame(encoded.toarray()), data[numerical_features]], axis=1)
 
 # Load the saved model using the load_model() function
-with open('/home/jane/Documents/Fraud-Detection-in-Electricity-and-Gas-Consumption-in-tunisia/model.pkl', 'rb') as f:
+with open('model.pkl', 'rb') as f:
     fraud_model = pkl.load(f)
 
 
